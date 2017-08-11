276e31aee85ebff9376d6bb3cc6d98e96d7c55d5
Valentin <grvalentin12@gmail.com>
2017-08-11 19:56:37 +0300

    hello

---
+++ if_else_if.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "How old are you?";
+$handle = fopen ("php://stdin","r");
+$age = fgets($handle);
+
+if ($age >= 18) {
+    echo "Please come in!";
+} elseif (($age < 18) && ($age >= 16)) {
+    echo "I'll think about it";
+} else echo "GO AWAY!!!!!";
+?>
+
